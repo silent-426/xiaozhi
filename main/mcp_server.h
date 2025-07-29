ac7e2d891db4cbf09b4c6a319f35a9039e951d13
silent-426 <3384594382@qq.com>
2025-07-29 13:42:23 +0800

    已实现语音控制车灯同步档位切换助力调节功能

--- main/mcp_server.h
+++ main/mcp_server.h
@@ -250,7 +250,7 @@ public:
 };
 
 class McpServer {
-private:
+public:
     static McpServer& GetInstance() {
         static McpServer instance;
         return instance;
@@ -262,7 +262,10 @@ private:
     void ParseMessage(const cJSON* json);
     void ParseMessage(const std::string& message);
     //SerialDriver serial_driver_;
-public:
+private:
+    McpServer();
+    ~McpServer();
+
     void ParseCapabilities(const cJSON* capabilities);
 
     void ReplyResult(int id, const std::string& result);
@@ -280,15 +283,6 @@ public:
   QueueHandle_t uart_queue_{ nullptr };
   static constexpr uart_port_t VCU_PORT = UART_NUM_2;
   static constexpr int UART_BUF_SIZE = 1024;
-  TaskHandle_t twai_task_handle;  // CAN 任务句柄
-  
-  // TWAI 回调声明
-  static bool TwaiRxCallback(twai_handle_t handle, 
-                           const twai_rx_event_data_t* edata,
-                           void* user_ctx);
-  
-  // TWAI 初始化方法声明
-  void SetupTwaiEvent();
 };
 
 #endif // MCP_SERVER_H
